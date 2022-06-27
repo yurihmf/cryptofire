62b145efd456405e4b53166d6ca17aafd4714c6f
Yuri Ferreira <yuri.ferreira@ativo.com>
2022-06-27 11:28:41 -0300

    add preconnect from google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import React from 'react'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 const Document = () => {
   return (
     <Html>
         <Head>
             <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-            <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+            <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
             <Main />
