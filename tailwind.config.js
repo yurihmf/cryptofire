362e312cf99cbf4ba88e5229da955bbdf09b08fc
Yuri Ferreira <yuri.ferreira@ativo.com>
2022-06-27 11:36:45 -0300

    added a secondary font

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      'nunito': ['Nunito']
+      'nunito': ['Nunito', 'sans-serif']
     },
     colors: {
       'background': '#fffffe',
