8c13fde128ac59981634929ae08489ca18c6cf19
Yuri Ferreira <yuri.ferreira@ativo.com>
2022-06-27 16:26:18 -0300

    added links on header

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,8 +1,9 @@
 
+
 const Footer = () => {
   return (
     <div className='bg-title flex items-center justify-center text-main py-4 font-nunito'>
-      2022 - By Yuri Ferreira
+      <span>2022 - By Yuri Ferreira</span>
     </div>
   )
 }
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,25 @@
 import Link from 'next/link'
-import React from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const Header = () => {
   return (
     <header className='bg-title'>
-      <div className='transition-all delay-75 ease-in-out container mx-auto flex justify-center items-center md:justify-between text-white'>
+      <div className='transition-all delay-75 ease-in-out container mx-auto flex justify-around items-center md:justify-between text-white'>
         <Link href='/'>
           <span className='text-xl text-main py-3 font-nunito cursor-pointer'>❤️‍🔥 CryptoFire</span>
         </Link>
+        <div className='flex gap-x-2 md:gap-x-4'>
+          <Link href='https://github.com/yurihmf/cryptofire'>
+            <span className='cursor-pointer transition-all delay-75 hover:scale-110'>
+              <FaGithub color='#fff' size={25}/>
+            </span>
+          </Link>
+          <Link href='https://www.linkedin.com/in/yuri-homen-de-mello-ferreira-b04232191/'>
+            <span className='cursor-pointer transition-all delay-75 hover:scale-110'>
+              <FaLinkedin color='#fff' size={25} />
+            </span>
+          </Link>
+          </div>
       </div>
     </header>
   )
